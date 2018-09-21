7a5ecc5965a4ad1246964e29b70f2c09365bf1d9
ahmet.bozkurt <b.ahmetrasim@gmail.com>
2018-09-22 01:28:25 +0300

    Utilities.sheetname ayarlama

--- Order Solutions/Utilities.cs
+++ Order Solutions/Utilities.cs
@@ -9,9 +9,13 @@ namespace Order_Solutions
     class Utilities
     {
         // Split Methodunu yaz 
+        // tek tırnak char // çift tırnak string 
         public static string Trimsheetnames (string Sheetname)
         {
+            if (Sheetname.Split(' ').Length == 1)
+            {
 
+            } 
 
             return "a";
         }
